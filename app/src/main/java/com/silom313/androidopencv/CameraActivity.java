cb792d5193e37b0d9af74679209253b64f8f4e0f
Wasith Theerapattrathamrong <phaicpe@gmail.com>
2016-05-28 22:04:09 +0700

    Change Activity name from MainActivity to CameraActivity also layout from activity_main to activity_camera

--- app/src/main/java/com/silom313/androidopencv/CameraActivity.java
+++ app/src/main/java/com/silom313/androidopencv/CameraActivity.java
@@ -1,13 +1,13 @@
 package com.silom313.androidopencv;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class CameraActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_camera);
     }
 }
